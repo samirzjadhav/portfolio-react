3dfa52c4f3692595695b8ba9a121f5d0273a2606
Samir Jadhav <samirzjadhav@gmail.com>
2024-03-28 09:50:58 -0700

    fix: icons package

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -11,33 +11,28 @@ export default function Navbar() {
           <div className="nav-menu" id="nav-menu">
             <ul className="nav-list">
               <li className="nav-item center">
-                <i className="bx bx-home"></i>
                 <a href="#home" className="nav-link active">
                   {" "}
                   Home
                 </a>
               </li>
               <li className="nav-item center">
-                <i className="bx bx-user"></i>
                 <a href="#about" className="nav-link">
                   About
                 </a>
               </li>
               <li className="nav-item center">
-                <i className="bx bx-code-alt"></i>
                 <a href="#skills" className="nav-link">
                   {" "}
                   Skills
                 </a>
               </li>
               <li className="nav-item center">
-                <i className="bx bx-window"></i>
                 <a href="#portfolio" className="nav-link">
                   Portfolio
                 </a>
               </li>
               <li className="nav-item center">
-                <i className="bx bx-user-circle"></i>
                 <a href="#contact" className="nav-link">
                   Contact
                 </a>
